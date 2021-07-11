0fbd7ed78298560d5121dc215911db672a7deda3
Parents <garulfo@azules.eu>
2021-07-11 23:15:45 +0200

    Minor updates

--- fr2/compile.sh
+++ fr2/compile.sh
@@ -1,5 +1,5 @@
-context --mode=screen introCTX_fra --result=introCTX_fra_s
-context               introCTX_fra --result=introCTX_fra_p
+context              introCTX_fra --result=introCTX_fra_s
+context --mode=print introCTX_fra --result=introCTX_fra_p
 
 pdftocairo -png -r 240 -scale-to 800 -singlefile introCTX_fra_s.pdf introCTX_fra
 
