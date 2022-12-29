a0cb81a85132ee10c0839dcbea134451daf1a43d
Parents <parents@GNULinuxDesktop2>
2022-12-29 16:18:19 +0100

    second

--- fr/compile.sh
+++
@@ -1,7 +0,0 @@
-context --mode=screen introCTX_fra --result=introCTX_fra_s
-context               introCTX_fra --result=introCTX_fra_p
-
-pdftocairo -png -r 240 -scale-to 800 -singlefile introCTX_fra_s.pdf introCTX_fra
-
-#pdftocairo -png -r 240 -scale-to 800 -singlefile introCTX_esp.pdf introCTX_esp
-#pdftocairo -png -r 240 -scale-to 800 -singlefile introCTX_eng.pdf introCTX_eng
--- ru/compile.sh
+++
@@ -1,9 +0,0 @@
-context --mode=screen introCTX_rus.mkiv --result=introCTX_rus
-# context --mode=screen introCTX_rus.mkiv --result=introCTX_rus_s
-# context               introCTX_rus.mkiv --result=introCTX_rus_p
-
-pdftocairo -png -r 240 -scale-to 800 -singlefile introCTX_rus.pdf introCTX_rus
-# pdftocairo -png -r 240 -scale-to 800 -singlefile introCTX_rus_s.pdf introCTX_rus
-
-# pdftocairo -png -r 240 -scale-to 800 -singlefile introCTX_esp.pdf introCTX_esp
-# pdftocairo -png -r 240 -scale-to 800 -singlefile introCTX_eng.pdf introCTX_eng
